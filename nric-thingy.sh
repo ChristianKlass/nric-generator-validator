2f0c90e1ba0a95181644485c7b6e49e77843153c
Mark Klass <mark_christian@zoho.com>
2022-02-12 18:31:18 +0800

    add GetChecksum() function with fake NRIC (I hope) to validate. NRIC: S1234567 - Should output a D. Verified with https://samliew.com/singapore-nric-validator.

--- nric-thingy.sh
+++ nric-thingy.sh
@@ -5,8 +5,50 @@ DisplayHelp() {
   echo -e "\tThis will generate <num> NRICs."
 }
 
-GenerateNRIC() {
-  prefix=$1
+# Take for example the NRIC number S1234567. 
+# first digit you multiply by 2, 
+# second multiply by 7, 
+# third by 6, 
+# fourth by 5, 
+# fifth by 4, 
+# sixth by 3, 
+# seventh by 2. 
+# Add the totals together. 
+# So 1×2 + 2×7 + 3×6 + 4×5 + 5×4 + 6×3 + 7×2 = 106
+
+GetChecksum() {
+  prefixIsSorT=("J" "Z" "I" "H" "G" "F" "E" "D" "C" "B" "A")
+  prefixIsForG=("X" "W" "U" "T" "R" "Q" "P" "N" "M" "L" "K")
+
+  PREFIX=$2
+  # echo "==============="
+  let SUM=0
+
+  for (( i = 0; i < ${#1}; ++i )); do
+    if [[ "$i" == 0 ]]; then
+      let CURR="$((2 * ${1:$i:1}))"
+      let SUM+="$CURR"
+    elif [[ $i -gt 0 ]]; then 
+      let CURR="$(((8 - $i) * ${1:$i:1}))"
+      let SUM+="$CURR"
+    fi
+  done
+
+  echo $SUM
+  if [[ "$PREFIX" == "T" || "$PREFIX" == "G" ]]; then
+    let SUM+=4
+  fi
+
+  let RMD="$(($SUM % 11))"
+
+  if [[ "$PREFIX" == "S" || "$PREFIX " == "T" ]]; then
+    echo ${prefixIsSorT[$RMD]}
+  elif [[ "$PREFIX" == "F" || "$PREFIX " == "G" ]]; then
+    echo ${prefixIsForG[$RMD]}
+  fi
+}
+
+GenerateNumber() {
   digits=7
 
   rand=$(od -A n -t d -N 2 /dev/urandom |tr -d ' ')
@@ -16,7 +58,11 @@ GenerateNRIC() {
     num="${num}$((rand % 10))"
   done
 
-  echo $prefix$num
+  echo $num
 }
 
-GenerateNRIC F
+# Verified with online verification thingy - Should return a D.
+GetChecksum '1234567' 'S'
+
+# GenerateNumber F
+# GetChecksum $(GenerateNumber F)
